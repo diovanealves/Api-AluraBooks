3e213057f4eb067475dc29621c70398753cd720d
Diovane Alves <alvdiovane@gmail.com>
2023-09-04 23:15:40 -0300

    feat: Enhancing validation code with yup

--- src/interface/AuthorInterface.ts
+++ src/interface/AuthorInterface.ts
@@ -1,11 +1,20 @@
 import * as yup from 'yup'
 
 export const AuthorSchemaCreate = yup.object({
-  name: yup.string().required('name is required'),
-  nationality: yup.string().required('nationality is required'),
+  name: yup
+    .string()
+    .required('name is required')
+    .matches(/\S/, 'name was provided blank'),
+  nationality: yup
+    .string()
+    .required('nationality is required')
+    .matches(/\S/, 'nationality was provided blank'),
 })
 
 export const AuthorSchemaUpdate = yup.object({
-  name: yup.string().optional(),
-  nationality: yup.string().optional(),
+  name: yup.string().optional().matches(/\S/, 'name was provided blank'),
+  nationality: yup
+    .string()
+    .optional()
+    .matches(/\S/, 'nationality was provided blank'),
 })
--- src/interface/BookInterface.ts
+++ src/interface/BookInterface.ts
@@ -2,19 +2,72 @@ import * as yup from 'yup'
 import mongoose from 'mongoose'
 
 export const BookSchemaCreate = yup.object({
-  title: yup.string().required('title is required'),
-  description: yup.string().required('description is required'),
+  title: yup
+    .string()
+    .required('title is required')
+    .matches(/\S/, 'title was provided blank'),
+  description: yup
+    .string()
+    .required('description is required')
+    .matches(/\S/, 'description was provided blank'),
   author: yup
     .string()
     .required('author is required')
     .test('is-object-id', 'author must be a valid ObjectId', (value) => {
       return mongoose.Types.ObjectId.isValid(value)
-    }),
-  publisher: yup.string().required('publisher is required'),
+    })
+    .matches(/\S/, 'author was provided blank'),
+  publisher: yup
+    .string()
+    .required('publisher is required')
+    .test(
+      'enum',
+      `the publisher provided is not a permitted value`,
+      (value) => {
+        if (!['Casa do Código', 'Alura'].includes(value)) {
+          throw new yup.ValidationError(
+            `the Publisher ${value} provided is not a permitted value`,
+          )
+        }
+        return true
+      },
+    )
+    .matches(/\S/, 'publisher was provided blank'),
+  numberPages: yup
+    .number()
+    .min(10, 'The number of pages should be between 10 and 5000')
+    .max(5000, 'The number of pages should be between 10 and 5000')
+    .required('Number of pages required'),
 })
 
 export const BookSchemaUpdate = yup.object({
-  title: yup.string().optional(),
-  description: yup.string().optional(),
-  publisher: yup.string().optional(),
+  title: yup
+    .string()
+    .optional()
+    .matches(/\S/, 'nationality was provided blank'),
+  description: yup
+    .string()
+    .optional()
+    .matches(/\S/, 'nationality was provided blank'),
+  publisher: yup
+    .string()
+    .optional()
+    .test(
+      'enum',
+      'the publisher provided is not a permitted value',
+      (value) => {
+        if (value && !['Casa do Código', 'Alura'].includes(value)) {
+          throw new yup.ValidationError(
+            `the Publisher ${value} provided is not a permitted value`,
+          )
+        }
+        return true
+      },
+    )
+    .matches(/\S/, 'nationality was provided blank'),
+  numberPages: yup
+    .number()
+    .min(10, 'The number of pages should be between 10 and 5000')
+    .max(5000, 'The number of pages should be between 10 and 5000')
+    .optional(),
 })
